54af26b322ac2ac7d09a5f47cd6b38a58ae5f0cf
Brad Hodge <99879327+hodge-b@users.noreply.github.com>
2026-05-27 20:23:04 -0400

    feat(ZOS-2): added basic text to vga buffer (#2)

--- kernel.c
+++ kernel.c
@@ -1,3 +1,38 @@
-void kernel_main(void) {
-  while(1) {}
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+
+typedef struct {
+  unsigned char value;
+  unsigned char color;
+} vga_entry_t;
+
+#define VGA_MEMORY 0xB8000
+#define VGA_COLOR 0x0F
+#define VGA_CLEAR 0x00
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+void vga_buffer_init(uint16_t* vga) {
+  uint16_t size = VGA_WIDTH * VGA_HEIGHT;
+  for (uint16_t i = 0; i < size; i++) {
+    vga[i] = ' ' | VGA_COLOR << 8;
+  }
 }
+
+void kernel_main(void) {
+  uint16_t* vga = (uint16_t*)VGA_MEMORY;
+  vga_buffer_init(vga);
+
+  unsigned char message[] = "Hello ZeroOS!";
+
+  uint8_t i = 0;
+  while(message[i] != '\0') {
+    vga[i] = message[i] | VGA_COLOR << 8;
+    i++;
+  }
+
+  while (1) {}
+}
+
+
